4f65fc844d762fc348bd5fefaabad76e1e81fcec
HUJH511 <jinhai001@e.ntu.edu.sg>
2025-07-30 20:35:17 +0800

    Update cmdline

--- align/cmdline.py
+++ align/cmdline.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 class CmdlineParser():
 
     def __init__(self, *args, **kwargs):
-        align_home = os.environ.get('ALIGN_HOME', None)
+        align_home = os.environ.get("ALIGN_HOME", None)
 
         parser = argparse.ArgumentParser(*args, **kwargs,
             description="directory path for input circuits")
@@ -66,12 +66,14 @@ class CmdlineParser():
         #                     action='store_true',
         #                     help="Set the true to generate png")
         log_level, verbosity = logmanager.get_loglevels()
-        parser.add_argument("-l", "--log",
+        parser.add_argument("-l",
+                            "--log",
                             dest="log_level",
                             choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'],
                             default=log_level,
                             help="Logfile logging level (default: %(default)s)")
-        parser.add_argument("-v", "--verbosity",
+        parser.add_argument("-v",
+                            "--verbosity",
                             dest="verbosity",
                             choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'],
                             default=verbosity,
@@ -88,100 +90,81 @@ class CmdlineParser():
                             "--PDN_mode",
                             action='store_true',
                             help='Set to true to run power delivery network code (Default False)')
-        parser.add_argument('--version',
+        parser.add_argument("--version",
                             action='version',
                             version='%(prog)s ' + __version__)
-
-        parser.add_argument('--flow_start',
+        parser.add_argument("--flow_start",
                             type=str,
                             help='Stage to start the flow. Previous stages are skipped.')
-        parser.add_argument('--flow_stop',
+        parser.add_argument("--flow_stop",
                             type=str,
                             help='Stage after which to stop the flow. Subsequent stages are skipped.')
-
-        parser.add_argument('--router_mode',
+        parser.add_argument("--router_mode",
                             type=str,
                             default='top_down',
                             choices=['top_down','bottom_up','collect_pins','no_op'],
                             help='Router mode')
-
-        parser.add_argument('--router',
+        parser.add_argument("--router",
                             type=str,
                             default='astar',
                             choices=['astar','hanan'],
                             help='Router to use')
-
-        parser.add_argument('--gui',
+        parser.add_argument("--gui",
                             action='store_true',
                             help='Run in GUI mode')
-
-        parser.add_argument('--port',
+        parser.add_argument("--port",
                             type=int,
                             action=None,
                             help='Port for GUI mode')
-
-        parser.add_argument('--skipGDS',
+        parser.add_argument("--skipGDS",
                             action='store_true',
                             help='Don\'t generate GDS files.')
-
-        parser.add_argument('--lambda_coeff',
+        parser.add_argument("--lambda_coeff",
                             type=float,
                             default=1.0,
                             help='Multiplier for hpwl in placer cost function.')
-
-        parser.add_argument('--nroutings',
+        parser.add_argument("--nroutings",
                             type=int,
                             default=1,
                             help='Maximum number of routings to generate.')
-
-        parser.add_argument('--viewer',
+        parser.add_argument("--viewer",
                             action='store_true',
                             help='Start lightweight viewer.')
-
-        parser.add_argument('--select_in_ILP',
+        parser.add_argument("--select_in_ILP",
                             action='store_true',
                             help='Use ILP to determine subcircuit selection.')
-
-        parser.add_argument('--place_using_ILP',
+        parser.add_argument("--place_using_ILP",
                             action='store_true',
                             help='Use ILP to determine subcircuit selection.')
-
-        parser.add_argument('--use_analytical_placer',
+        parser.add_argument("--use_analytical_placer",
                             action='store_true',
                             help='Use analytical placer.')
-
-        parser.add_argument('--placer_sa_iterations',
+        parser.add_argument("--placer_sa_iterations",
                             type=int,
                             default=10000,
                             help="Iterations used by the placer's SA algorithm.")
-
-        parser.add_argument('--seed',
+        parser.add_argument("--seed",
                             type=int,
                             default=0,
                             help='Random number generator seed for the placement algorithm')
-
-        parser.add_argument('--ilp_solver',
+        parser.add_argument("--ilp_solver",
                             type=str,
                             default='symphony',
                             choices=['symphony', 'lpsolve'],
                             help='ILP Solver used by placer ')
-
-        parser.add_argument('--placer_ilp_runtime',
+        parser.add_argument("--placer_ilp_runtime",
                             type=int,
                             default=1,
                             help="Runtime limit in seconds for ILP in each iteration of placement")
-
-        parser.add_argument('--placer',
+        parser.add_argument("--placer",
                             choices=['cpp', 'python'],
                             default='cpp',
                             help='Select the placer engine to use. Default: %(default)s')
-
-        parser.add_argument('-b',
-                            '--blackbox_dir',
+        parser.add_argument("-b",
+                            "--blackbox_dir",
                             default=None,
                             help='directory containing black box GDS files')
-
-        parser.add_argument('--scale',
+        parser.add_argument("--scale",
                             type=float,
                             default=1e3,
                             help='black box LEF scaling')
